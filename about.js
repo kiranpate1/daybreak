732ee4eabedc55de211b10cafeb25f0d70c452b3
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2022-07-05 21:41:14 -0400

    Update about.js

--- about.js
+++ about.js
@@ -21,6 +21,38 @@ $(function() {
     });
 });
 
+// Get the an HTML element
+var element = document.querySelector('#after-carousel');
+
+// Get its bounding client rectangle
+var bounding = element.getBoundingClientRect();
+
+function isInViewPort(element) {
+    // Get the bounding client rectangle position in the viewport
+    var bounding = element.getBoundingClientRect();
+
+    // Checking part. Here the code checks if it's *fully* visible
+    // Edit this part if you just want a partial visibility
+    if (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    ) {
+        console.log('In the viewport! :)');
+        return true;
+    } else {
+        console.log('Not in the viewport. :(');
+        return false;
+    }
+}
+
+window.addEventListener('scroll', function (event) {
+    if (isInViewport(theElementToWatch)) {
+      // update the element display
+    }
+}, false);
+
 var elementDelay = 100;
 setTimeout(function () {
 	for (let i = 0; i < document.querySelectorAll('.heading-massive span').length; i++) {
