06de9ec8006608091acc3396aaf43df4c73ca030
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2022-07-05 23:11:34 -0400

    Update about.js

--- about.js
+++ about.js
@@ -37,7 +37,7 @@ const box = document.querySelector('#after-carousel');
 
 document.addEventListener('scroll', function () {
 	if (isInViewport(box) === true) {
-		var elementDelay = 100;
+		var elementDelay = 500;
 			for (let i = 0; i < document.querySelectorAll('.heading-massive span').length; i++) {
 				document.querySelectorAll('.heading-massive span').forEach((element, i) => {
 					setTimeout(function () {
