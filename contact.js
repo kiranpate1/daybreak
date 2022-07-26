f298cf54151c50d42b85768b36cb1995d0439607
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2022-07-25 23:47:02 -0400

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var elementDelay = 100;
+  var elementDelay = 75;
   for (let i = 0; i < document.querySelectorAll('.contact-heading span').length; i++) {
     document.querySelectorAll('.contact-heading span').forEach((element, i) => {
       setTimeout(function () {
