dc7fa4d3bb5458c9369aa0133ec0b32b141ee71c
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2022-08-08 01:27:00 -0400

    Update home.js

--- home.js
+++ home.js
@@ -79,6 +79,16 @@ window.onscroll = function (e) {
 		
     document.querySelectorAll('.layout-' + number + ' .desktop .project')[i].classList.add(combinedshuffle[i].slice(0, -2), combinedshuffle[i]);
   }
+	$(".inspired").attr("href", "/inspired")
+	$(".party-round").attr("href", "/party-round")
+	$(".prologue").attr("href", "/prologue")
+	$(".wombo-dream").attr("href", "/wombo-dream")
+	$(".workweek").attr("href", "/workweek")
+	$(".hyper").attr("href", "/hyper")
+	$(".notes-about-people").attr("href", "/notes-about-people")
+	$(".party-grounds").attr("href", "/party-grounds")
+	$(".startup-supreme").attr("href", "/startup-supreme")
+	$(".vc-puzzle").attr("href", "/vc-puzzle")
 
 	for (let i = 0; i < document.querySelectorAll('.inspired').length; i++) {
 		document.querySelectorAll('.inspired .project-hover .caption')[i].innerHTML = "2022";
